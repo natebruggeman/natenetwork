12dbab6ac29c77f7e72e023612f8bb8dfc211933
Nate Bruggeman <njbruggeman@gmail.com>
2020-09-24 11:49:07 -0500

    Get routes running for auth, users, profile, and posts

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const connectDB = require('./config/db')
 
 const app = express()
 
+
+// connect database
 connectDB();
 
 
@@ -15,6 +17,14 @@ connectDB();
 
 
 app.get('/', (req, res) => res.send('API running'))
+
+//Define routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
+
+
 
 const PORT = process.env.PORT || 5000
 
